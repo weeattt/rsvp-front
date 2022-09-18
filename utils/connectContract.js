f06f6734d7d6d4396e8e119ba195c7d5369a9277
weeattt <weeat0420@naver.com>
2022-09-19 03:13:00 +0900

    second commit

---
+++ utils/connectContract.js
@@ -0,0 +1,26 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+    //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
+    const contractAddress = "0x6da07b15D3d01b36040C78D0a181075326ec3f9F";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        //checking for eth object in the window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+    return rsvpContract;
+  }
+  
+  export default connectContract;
